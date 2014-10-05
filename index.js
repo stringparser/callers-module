f0fd666d74edd7ee3a18a9f32bf69dcfe95df0fe
Javier Carrillo <stringparser@gmail.com>
2014-10-05 13:19:45 +0200

    condense

--- index.js
+++ index.js
@@ -10,10 +10,9 @@ function callersModule(frames, origin){
   var stack = callsites(frames, origin || callersModule);
   var frame = stack[stack.length-1];
 
-  var filename = frame.getFileName();
   var line = frame.getLineNumber();
+  var filename = frame.getFileName();
   var column = frame.getColumnNumber();
-
   var location = filename + ':' + line + ':' + column;
 
   var ext = path.extname(filename);
@@ -36,16 +35,10 @@ function callersModule(frames, origin){
 
   var dir = filename.split(path.sep);
   var index = dir.indexOf('node_modules');
-  if( index < 0 ){
-    // is your code
+  if( index < 0 ){ // is your code
 
-    moduleName = path.relative(
-      path.resolve(wd, '..'), wd
-    );
-
-    scopeName = path.relative(
-      wd, path.dirname(filename)
-    ) || './.';
+    moduleName = path.relative( path.resolve(wd, '..'), wd);
+     scopeName = path.relative( wd, path.dirname(filename)) || './.';
 
     return {
        module : moduleName,
@@ -53,8 +46,8 @@ function callersModule(frames, origin){
          path : filename,
      location : location
     };
-  }
-  // its in `node_modules`
+  } // then its in `node_modules`
+
   moduleName = dir[index+1];
   scopeName = dir;
 
@@ -63,9 +56,7 @@ function callersModule(frames, origin){
     index = scopeName.indexOf('node_modules');
   }
 
-  scopeName = dir.slice(
-    dir.indexOf('node_modules') + 1, index
-  ).join(path.sep);
+  scopeName = dir.slice( dir.indexOf('node_modules') + 1, index).join(path.sep);
 
   return {
      module : moduleName,
